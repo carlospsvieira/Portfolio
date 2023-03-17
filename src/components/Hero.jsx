58807692454d7cd412d31e4a9969f27ab14d5dec
Carlos Vieira <carlepsvieira@gmail.com>
2023-03-17 19:44:22 -0300

    adds github and linkedin icons to navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,12 +7,13 @@ function Hero() {
       <div className="w-[80%] h-screen border-x-[2px] border-t-[2px] border-zinc-600 hero">
         <div className="flex flex-col items-center text-white gap-4">
           <h2 className="heading-title">WEB DEVELOPER</h2>
-          <span>Glad you're here</span>
+          <span>Glad you're here!</span>
           <p>
             Welcome to my portfolio! I am a skilled and dedicated developer with
-            a passion for creating visually stunning and functional applications.
-            Explore my portfolio, which is one of my projects, to see examples
-            of my work and learn more about my experience and skills.
+            a passion for creating visually stunning and functional
+            applications. Explore my portfolio, which is one of my projects, to
+            see examples of my work and learn more about my experience and
+            skills.
           </p>
           <a href="history" className="bg-red-500 py-2 px-4">
             MY STORY
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { SiLinkedin, SiGithub } from "react-icons/si";
 
 function Navbar() {
   return (
     <nav className="flex justify-between text-white text-xl py-6 w-[75%]">
       <div className="flex items-center gap-8">
-        <a href="" className="bg-red-500 py-2 px-4">HOME</a>
+        <a href="" className="bg-red-500 py-2 px-4">
+          HOME
+        </a>
         <a href="">MY STORY</a>
         <a href="">PROJECTS</a>
         <a href="">CONTACT</a>
       </div>
       <div className="flex items-center gap-8">
-        <a href="">LINKEDIN</a>
-        <a href="">GITHUB</a>
+        <a href="">
+          {" "}
+          <SiLinkedin size={35} />{" "}
+        </a>
+        <a href="">
+          {" "}
+          <SiGithub size={35} />{" "}
+        </a>
       </div>
     </nav>
   );
