31cc1fe4a0d9ea750dfc8d7e088312e44b63b1f9
Carlos Vieira <carlepsvieira@gmail.com>
2023-03-17 11:26:54 -0300

    adds nav links and hero placeholding text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 function Hero() {
   return (
-    <div>
-      <h1>hero</h1>
-    </div>
+    <>
+      <h1>Carlos Vieira</h1>
+      <div>
+        <h2>Web</h2>
+        <h2>Developer</h2>
+        <span>something here</span>
+        <p>a text goes here a little longer but not much.</p>
+        <a href="#history">My Journey</a>
+      </div>
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Navbar() {
-  return <nav>Navbar</nav>;
+  return (
+    <nav>
+      <div>
+        <a href="">Home</a>
+        <a href="">My Story</a>
+        <a href="">Projects</a>
+        <a href="">Contact</a>
+      </div>
+      <div>
+        <a href="">linkedin</a>
+        <a href="">github</a>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
