1d056a63344cd1f7f2b50974efb07bb875cf4ce8
Carlos Vieira <carlepsvieira@gmail.com>
2023-03-17 18:57:20 -0300

    adds hero styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,19 @@ function Hero() {
   return (
     <>
       <h1 className="heading-name">Carlos Vieira</h1>
-      <div className="w-[80%] h-screen border-x-[2px] border-t-[2px] border-zinc-600 rounded-md hero">
-        <div className="flex flex-col items-center text-white">
-          <h2 className="heading-title">Web Developer</h2>
-          <span>something here</span>
-          <p>a text goes here a little longer but not much.</p>
-          <a href="#history">My Journey</a>
+      <div className="w-[80%] h-screen border-x-[2px] border-t-[2px] border-zinc-600 hero">
+        <div className="flex flex-col items-center text-white gap-4">
+          <h2 className="heading-title">WEB DEVELOPER</h2>
+          <span>Glad you're here</span>
+          <p>
+            Welcome to my portfolio! I am a skilled and dedicated developer with
+            a passion for creating visually stunning and functional applications.
+            Explore my portfolio, which is one of my projects, to see examples
+            of my work and learn more about my experience and skills.
+          </p>
+          <a href="history" className="bg-red-500 py-2 px-4">
+            MY STORY
+          </a>
         </div>
       </div>
     </>
