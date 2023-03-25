3b9c5ca62cc570e169b56276bfac57be23957bd4
Carlos Vieira <carlepsvieira@gmail.com>
2023-03-24 22:32:21 -0300

    nav styles when media query

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { SiLinkedin, SiGithub } from "react-icons/si";
 function Navbar() {
   return (
-    <nav className="flex justify-between text-white text-xl py-6 w-[75%] fixed z-10">
-      <div className="flex items-center gap-8 lg-screen-nav">
+    <nav className="flex justify-between text-white text-xl py-6 w-[75%] lg:fixed z-10">
+      <div className="flex items-center gap-8">
         <a href="#home" className="bg-red-500 py-2 px-4">
           HOME
         </a>
-        <a href="#story">MY STORY</a>
-        <a href="#projects">PROJECTS</a>
-        <a href="#contact">CONTACT</a>
+        <a href="#story" className="lg-screen-nav">MY STORY</a>
+        <a href="#projects" className="lg-screen-nav">PROJECTS</a>
+        <a href="#contact" className="lg-screen-nav">CONTACT</a>
       </div>
-      <div className="flex items-center gap-8 lg-screen-nav">
+      <div className="flex items-center gap-1 lg:gap-8">
         <a href="https://www.linkedin.com/in/carlospsvieira/" target="_blank">
           {" "}
           <SiLinkedin size={35} />{" "}
